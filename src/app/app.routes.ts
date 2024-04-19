e66dd6a1e7ff41c08c9f45fb768a6005e8d172b4
Sznotti2 <aztelfelejtettem@gmail.com>
2024-04-19 21:41:22 +0200

    generated basic components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,40 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { TorrentsComponent } from './components/torrents/torrents.component';
+import { UploadComponent } from './components/upload/upload.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: 'home',
+		component: HomeComponent
+	},
+	{
+		path: 'login',
+		component: LoginComponent
+	},
+	{
+		path: 'signup',
+		component: SignupComponent
+	},
+	{
+		path: 'torrents',
+		component: TorrentsComponent
+	},
+	{
+		path: 'upload',
+		component: UploadComponent
+	},
+	{
+		path: '',
+		redirectTo: 'home',
+		pathMatch: 'full'
+	},
+	{
+		path: '**',
+		pathMatch: 'full',
+		component: NotFoundComponent
+	},
+];
---
+++ src/app/components/torrents/torrents.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-torrents',
+  standalone: true,
+  imports: [],
+  templateUrl: './torrents.component.html',
+  styleUrl: './torrents.component.css'
+})
+export class TorrentsComponent {
+
+}
