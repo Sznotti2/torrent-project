82ea58b1f45aee4fe6b2705c8d3cae48cff46cf3
Sznotti2 <aztelfelejtettem@gmail.com>
2024-05-01 18:37:41 +0200

    keresés megvalósítás elkezdése

--- src/app/components/torrents/torrents.component.ts
+++ src/app/components/torrents/torrents.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit, inject } from '@angular/core';
 
 @Component({
   selector: 'app-torrents',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule],
   templateUrl: './torrents.component.html',
   styleUrl: './torrents.component.css'
 })
-export class TorrentsComponent {
+export class TorrentsComponent implements OnInit {
 
+	httpClient = inject(HttpClient);
+	data: any[] = [];
+
+
+	ngOnInit(): void {
+		this.fetchData();
+	}
+
+	fetchData(): void {
+		// this.httpClient.get('https://jsonplaceholder.typicode.com/posts').subscribe((data: any) => {
+		// 	console.log(data);
+		// 	this.data = data;
+		// });
+
+		this.httpClient.get('https://1337xto/api/v1/search?site=1337x&query=avengers').subscribe((data: any) => {
+			console.log(data);
+			this.data = data;
+		});
+	}
 }
+// api/v1/search?site=1337x&query=avengers
